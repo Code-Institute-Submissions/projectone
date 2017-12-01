8dab6b90a7b7b01451206639849baa833444775e
Gillian <gillianohea@gmail.com>
2017-12-01 10:00:49 +0000

    fri morn updates

--- js/script.js
+++ js/script.js
@@ -1,6 +1,76 @@
-$("img").mouseenter(function(){
+/*$(document).ready(function() { 
 
-}
-$("img").mouseleave(function(){
-	
-}
+
+$('text').hide().removeClass('text').addClass('text-js');
+
+$('thumb').hover(function(){
+	$(this).find('.text-js').fadeToggle();
+});*/
+
+
+
+$("thumb").mouseover(function(){
+	$("text").show();
+});
+
+$("thumb").mouseout(function(){
+	$("#text").hide();
+ });
+
+   /*$("button").mouseenter(function(){
+        $('button').toggle('medium');
+    });
+
+   $("button").mouseleave(function(){
+        $('button').toggle('medium');
+    });
+
+
+
+
+//$('button').mouseenter(function() {
+	//$(this).removeClass('damn').addClass('closeUp');
+
+ //});
+ 
+//$('button').mouseleave(function() { 
+//	$(this).removeClass('closeUp').addClass('damn');
+
+ //});
+
+
+//$('img').mouseenter(function() {
+	////$(this).removeClass('img-responsive').addClass('closeUp');
+
+//
+ 
+//$('img').mouseleave(function() { 
+	///$(this).removeClass('closeUp').addClass('img-responsive');
+
+ //});
+
+//$("#eight").mouseenter(function(){
+        //$("#one").addClass("smaller");
+        //$("#two").addClass("smaller");
+        //$("#three").addClass("smaller");
+        //$("#four").addClass("smaller");
+        //$("#five").addClass("smaller");
+        //$("#six").addClass("smaller");
+        //$("#seven").addClass("smaller");
+        //$("#eight").addClass("closeUp");
+
+    //});
+
+//$("#eight").mouseleave(function(){
+        //$("#one").removeClass("smaller");
+        //$("#two").removeClass("smaller");
+        //$("#three").removeClass("smaller");
+        //$("#four").removeClass("smaller");
+        //$("#five").removeClass("smaller");
+        //$("#six").removeClass("smaller");
+        //$("#seven").removeClass("smaller");
+        //$("#eight").removeClass("closeUp");
+
+    //});
+
+});
