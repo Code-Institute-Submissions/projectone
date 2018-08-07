0e0158b65a92b5308b33a5186d68ca279a784191
oheag2 <33259797+oheag2@users.noreply.github.com>
2018-08-07 14:05:57 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-var slideIndex = 0;
+// function validateForm() {
+// 	var x = document.forms["myForm"]["fname"].value;
+// 	if(x== "") 
+// 		alert ("Name must be filled in");
+// 	return false;
+// 	}
+
+
+var myIndex = 0;
 carousel();
 
 function carousel() {
-	var i;
-	var x = document.getElementsByClassName("myPics");
-	for (i = 0; i < x.length; i++) {
-		x[i].style.display = "none";
-	}
-	slideIndex++;
-	if (slideIndex > x.length) {slideIndex = 1}
-	x[slideIndex-1].style.display = "block";
-	setTimeout(carousel, 5000);
+    var i;
+    var x = document.getElementsByClassName("myPics");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2000); // Change image every 2 seconds
 }
