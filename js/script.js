c16a72f00dd8cf7b0dc9a78723bc9134f23527bd
oheag2 <33259797+oheag2@users.noreply.github.com>
2018-08-07 14:06:22 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
-function validateForm() {
-	var x = document.forms["myForm"]["fname"].value;
-	if(x== "") 
-		alert ("Name must be filled in");
-	return false;
-	}
+// function validateForm() {
+// 	var x = document.forms["myForm"]["fname"].value;
+// 	if(x== "") 
+// 		alert ("Name must be filled in");
+// 	return false;
+// 	}
+
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("myPics");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2000); // Change image every 2 seconds
 }
